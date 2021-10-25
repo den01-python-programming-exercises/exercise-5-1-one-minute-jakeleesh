a5519df9cfd82ce4fc68e8f2540ebd91795ba902
Jake Lee <jake.lee@fdmgroup.com>
2021-10-25 20:53:37 +0800

    Attempts solution

--- src/exercise.py
+++ src/exercise.py
@@ -2,6 +2,11 @@ from timer import Timer
 
 def main():
     #write your code below this line
+    timer = Timer()
+
+    while True:
+        print(timer)
+        timer.advance()
 
 if __name__ == '__main__':
     main()
--- src/timer.py
+++ src/timer.py
@@ -1,13 +1,18 @@
-from src.clock_hand import ClockHand
+from clock_hand import ClockHand
 
 class Timer:
 
     def __init__(self):
-        self.secs = # implement the seconds variable
-        self.hunds = # implement the hundreths variable
+        self.secs = ClockHand(60)
+        self.hunds = ClockHand(100)
 
     def advance(self):
         # implement the advance method here
+        self.hunds.advance()
+
+        if (self.hunds.get_value() == 0):
+            self.secs.advance()
 
     def __str__(self):
         # implement the string representation here
+        return str(self.secs) + ":" + str(self.hunds)
